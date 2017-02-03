0bb7c44d071d5267ce0a21cb410c7e5997568710
v5est0r <75314031@qq.com>
2017-02-03 20:21:49 +0800

    Update buauty.py

--- buauty.py
+++ buauty.py
@@ -1,101 +1,85 @@
-# -*- coding: UTF-8 -*-
-#coding by v5est0r
-#根据500个美女页面每页对应的大图页面,再匹配下载大图
+# -*- coding: utf-8 -*-
+#coded by v5est0r
 
-import re,requests
+import re,requests,urllib
 
 from pyquery import PyQuery as pq
 
-
 headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:48.0) Gecko/20100101 Firefox/48.0',}
 
-def get_bueutyPage1(url):
-    res = pq(url, headers=headers)
-    #print type(res)
-    jpg = res('a')
+#http://www.5442.com/meinv/20150813/26143_9.html
+#http://www.5442.com/meinv/20170203/40483_14.html
 
-    page1 = []
-    for q in jpg.items():
-        q1 = q.attr('href')
-        m = re.match(r"(http://www.5442.com/meinv/){1}.+\/.+html", str(q1))
-        if m:
-            page1.append(q1)
-            page1 = list(set(page1))  # list元素去重
-    #for each in page1:
-        #print each
+def get_bueutyPage1(url):
+    try:
+        res = pq(url, headers=headers)
+        jpg = res('a')
+        page1 = []
+        for q in jpg.items():
+            q1 = q.attr('href')
+            m = re.match(r"(http://www.5442.com/meinv/){1}.+\/.+html", str(q1))
+            if m:
+                page1.append(q1)
+                page1 = list(set(page1))  # ist元素去重
+    except:
+        pass
     return page1
 
-
 all_beautyPage = []
-
-for i in range(2,4):
+for i in range(1, 490):
     url = "http://www.5442.com/meinv/list_1_" + str(i) + ".html"
-    print '第%s页抓到%d个大图首页' %(i,len(get_bueutyPage1(url)))
-    all_beautyPage += get_bueutyPage1(url)
+    print "正在匹配list1第%d页的图集首页"%i
+    try:
+        all_beautyPage += get_bueutyPage1(url)
+    except:
+        pass
 
 all_beautyPage = list(set(all_beautyPage))
+print '去重后总共得到 %s 个大图首页' %len(all_beautyPage)
 
 chan_imglist = []
 for each in all_beautyPage:
     each = each.replace(str(str(each).split("/")[5]).split(".")[0],str(str(each).split("/")[5]).split(".")[0] + '_i')
     chan_imglist.append(each)
-#print chan_imglist
+chan_imglist = list(set(chan_imglist))
 
-yuan_imglist = []
-for each in all_beautyPage:
-    each = each.replace(str(str(each).split("/")[5]).split(".")[0],str(str(each).split("/")[5]).split(".")[0] + '_1')
-    yuan_imglist.append(each)
+#http://www.5442.com/meinv/20150314/16576_i.html
+#每个大图系列提取大图并下载
+def get_img(img_html):
+    #print img_html
+    for i in range(5,7):
+        img_list = []
+        img_html1=img_html.replace('_i','_'+str(i))
+        try:
+            r = requests.get(img_html1, allow_redirects=False)
+            if r.status_code == 200:
+                print "正在匹配大图页:%s" % img_html1
+                html = pq(img_html1, headers=headers)
+                img = html('img')
+                for qq in img.items():
+                    qq1 = qq.attr('src')
+                    m = re.match(r"(http://image.ytqmx){1}.+!960.jpg", qq1)
+                    n = re.match(r"(http://){1}.+!960.jpg", qq1)
+                    if m or n:
+                        img_list.append(qq1)
+                img_list += img_list
+                img_list = list(set(img_list))
+                print "本页抓取到%d张大图" % len(img_list)
+                for pic_url in img_list:
+                    print('正在下载图片%s') % pic_url
+                    filename = pic_url.replace(r'/', 'v5est0r')
+                    try:
+                        data = urllib.urlretrieve(pic_url,filename)
+                    except:
+                        pass
+            else:
+                break
+        except:
+            pass
+    return;
 
-print '去重后总共得到 %s 个大图首页' %len(all_beautyPage)
 
+for each in chan_imglist:
+    get_img(each)
 
-
-#创建在大图首页里提取大图的函数
-def get_imgUrl(url):
-    img_list = []
-    headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:48.0) Gecko/20100101 Firefox/48.0',}
-    html = pq(url,headers=headers)
-    img = html('img')
-    for qq in img.items():
-        qq1 = qq.attr('src')
-        m = re.match(r"(http://pic.5442){1}.+\/[0-9]{2}\.jpg.+\.jpg", str(qq1))
-        if m:
-            img_list.append(qq1)
-    img_list = list(set(img_list))
-    return img_list
-
-#判断页面状态 并开始抓取大图链接
-'''
-for imgHtml in yuan_imglist:
-    r = requests.get(imgHtml, allow_redirects=False)
-    if r.status_code == 200:
-        for i in range(0, 40):
-            try:
-                for each in chan_imglist:
-                    each = each.replace('_i', "_" + str(i))
-                    print each
-                    get_imgUrl(each)
-                    print '本页抓取到%d张大图' % len(get_imgUrl(url))
-            except:
-                pass
-'''
-
-all_imglist = []
-for i in range(1, 3):
-    for each in chan_imglist:
-        each = each.replace('_i', "_" + str(i))
-        #print each
-        r = requests.get(each, allow_redirects=False)
-        if r.status_code==200:
-            #print each
-            get_imgUrl(each)
-            for imgUrl in get_imgUrl(each):
-                all_imglist.append(imgUrl)
-                all_imglist = list(set(all_imglist))
-            print "本页抓取到 %d 张大图" % len(get_imgUrl(each))
-    print "这个大图首页系列共抓取到 %d 张大图" %len(all_imglist)
-    all_imglist += all_imglist
-print "合计共抓取到 %d 张大图" %len(all_imglist)
-
-for each in all_imglist:
-    print each
+#get_img("http://www.5442.com/meinv/20150813/26143_i.html")
